18cd5eb76c5106bc8eeb7eb7aca674b2c415e66e
Qiurong <rongqiu_carl@qq.com>
2021-01-21 12:16:28 +0800

    add ListNode

---
+++ List/ListNode.java
@@ -0,0 +1,14 @@
+package List;
+
+/**
+ * @description:
+ * @author: Qr
+ * @create: 2021-01-21 10:27
+ **/
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
