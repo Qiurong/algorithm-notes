3d0b8fa7d3082bc58c69a443a76e05da0c109615
Qiurong <rongqiu_carl@qq.com>
2021-02-27 16:59:14 +0800

    add binary-search

--- binary_search/binary_search.java
+++ binary_search/binary_search.java
@@ -7,6 +7,23 @@ package binary_search;
  **/
 public class binary_search {
     public int search(int[] nums, int target) {
-
+        int start = 0;
+        int end = nums.length - 1;
+        int targetPos = -1;
+        int mid = (start + end) >> 1;
+        while(start <= end){
+            if(nums[mid] == target){
+                targetPos = mid;
+                break;
+            }
+            else if (nums[mid] > target){
+                end = mid;
+            }
+            else {
+                start = mid;
+            }
+            mid = (start + end) >> 1;
+        }
+        return targetPos;
     }
 }
