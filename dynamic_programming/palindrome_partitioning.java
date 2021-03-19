273c57a267ebf72f27d29506acdcbf748e9cecb5
Qiurong <rongqiu_carl@qq.com>
2021-03-19 13:59:50 +0800

    update palindrome-partitioning

--- dynamic_programming/palindrome_partitioning.java
+++ dynamic_programming/palindrome_partitioning.java
@@ -14,9 +14,6 @@ import java.util.Stack;
  **/
 public class palindrome_partitioning {
 
-    //回溯法不断去深度遍历, 遇到截取的符合则递归深度, 不复合的剪枝(continue)
-    //两层循环：外层for循环用于穷举一个一个遍历
-    //        内层深度递归判断是否符合
     public List<List<String>> partition(String s){
         //记录截取路径上的字符
         ArrayList<String> path = new ArrayList<>();
@@ -27,10 +24,19 @@ public class palindrome_partitioning {
             return palindrome_Strings;
         }
         char[] chars = s.toCharArray();
-        backtrack(chars, 0,path,palindrome_Strings);
+        //backtrack(chars, 0,path,palindrome_Strings);
+
+        //memo: 用于存储切割字串是否回文. [i][j]：chars[i..j]子串是否回文
+        int[][] memo = new int[len][len];
+        backtrack_withMermory(chars,0,path,palindrome_Strings,memo);
         return palindrome_Strings;
     }
 
+    //回溯法不断去深度遍历, 遇到截取的符合则递归深度, 不符合的剪枝(continue)
+    //两层循环：外层for循环用于穷举一个一个遍历
+    //        内层深度递归判断是否符合
+    //时间复杂度：O(n * 2^n) 总共至多有(2^n)中切割方案, 判断回文的操作时间复杂度为O(n).
+    //空间复杂度：O(n). 栈的size == 递归调用深度 至多为n.(不计算最终res占用的空间)
     public void backtrack(char[] chars,int startIndex,ArrayList<String> path, List<List<String>> palindrome_Strings){
         //递归终止条件
         if (startIndex >= chars.length){
@@ -41,10 +47,8 @@ public class palindrome_partitioning {
         //外层遍历, 一个个去截断原字符串
         for (int i = startIndex; i < chars.length; i++) {
             //截出来的[startIndex,i]为回文, 则把[startIndex,i]加入到path中
-            if (!is_palindrome(chars,startIndex,i)){
-                continue;
-
-            }else {
+            //不为回文, 则直接剪枝.
+            if (is_palindrome(chars,startIndex,i)){
                 String str = new String(chars,startIndex,i - startIndex + 1);
                 //把截断出来的str放入path中
                 path.add(str);
@@ -66,6 +70,35 @@ public class palindrome_partitioning {
             endIdex--;
         }
         return true;
+    }
+
+
+    //加入记忆化搜索, 用一个二维数组memo存储切割字串是否为回文. 这样就不用重复计算
+    //时间复杂度:
+    //空间复杂度：O(n^2). memo的空间
+    public void backtrack_withMermory(char[] chars, int startIndex,ArrayList<String> path, List<List<String>> palindrome_Strings, int[][] memo){
+        if (startIndex >= chars.length){
+            palindrome_Strings.add(new ArrayList<>(path));
+            return;
+        }
+        //[startIndex,i]
+        for (int i = startIndex; i < chars.length; i++) {
+            //memo: 2:非回文. 1:回文. 0:未计算
+            //这里先判断非回文是希望节省时间
+            if (memo[startIndex][i] == 2){
+                continue;
+            }
+            //[startIndex,i]子串是回文
+            if (memo[startIndex][i] == 1 || is_palindrome(chars,startIndex,i)){
+                memo[startIndex][i] = 1;
+                String str = new String(chars, startIndex, i-startIndex+1);
+                path.add(str);
+                backtrack_withMermory(chars,i + 1,path,palindrome_Strings,memo);
+                path.remove(path.size()-1);
+            }else {
+                memo[startIndex][i] = 2;
+            }
+        }
     }
 
 }
