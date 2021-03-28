a5cd58f1b29093ae38926aa1f5523ad0f6ac6edd
Qiurong <rongqiu_carl@qq.com>
2021-03-28 23:33:16 +0800

    update with my sloution

--- dynamic_programming/longest_increasing_subsequence.java
+++ dynamic_programming/longest_increasing_subsequence.java
@@ -6,13 +6,46 @@ package dynamic_programming;
  * @create: 2021-03-17 22:42
  **/
 public class longest_increasing_subsequence {
-    //从后往前迭代
+    //从后往前迭代, 算法错误.看了官方题解应该是从前往后迭代.
     public int lengthOfLIS_brutalForce(int[] nums) {
-        int maxIncrSeqLen = 0;
         int len = nums.length;
-        for (int i = len - 1; i >= 0; i--) {
-
+        //用于记录最长递增子序列的数值. dp[i] = nums[i...len-1]的最长递增子序列长度
+        int dp[] = new int[len];
+        //用于记录最长递增子序列的最小值. dp[i] = nums[i...len-1]的最长递增子序列的最小值
+        int min[] = new int[len];
+        //边界
+        dp[len-1] = 1;
+        min[len-1] = nums[len-1];
+        for (int i = len - 2; i >= 0; i--) {
+            if (nums[i] < min[i+1]){
+                dp[i] = dp[i+1] + 1;
+                min[i] = nums[i];
+            }
+            else {
+                boolean flag = false;
+                for (int j = i+2; j <= len-1 ; j++) {
+                    if (nums[i] < min[j]){
+                        int temp = dp[j] + 1;
+                        if (temp > dp[i+1]){
+                            dp[i] = temp;
+                            min[i] = nums[i];
+                        } else if (temp < dp[i+1]){
+                            dp[i] = dp[i+1];
+                            min[i] = nums[i];
+                        } else{
+                            dp[i] = dp[i+1];
+                            min[i] = Math.max(nums[i],nums[i]);
+                        }
+                        flag = true;
+                        break;
+                    }
+                }
+                if(!flag){
+                    dp[i] = dp[i+1];
+                    min[i] = min[i+1];
+                }
+            }
         }
-        return maxIncrSeqLen;
+        return dp[0];
     }
 }
