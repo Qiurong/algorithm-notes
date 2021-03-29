8b982d61e1382cc1fd871c9941e05c43a3b09799
Qiurong <rongqiu_carl@qq.com>
2021-03-29 14:38:53 +0800

    update palindrome-partitioning-ii

--- dynamic_programming/palindrome_partitioning_ii.java
+++ dynamic_programming/palindrome_partitioning_ii.java
@@ -56,5 +56,45 @@ public class palindrome_partitioning_ii {
         return true;
     }
 
+    //使用动态规划预处理
+    public int minCut_DP(String s){
+        int len = s.length();
+        if (len == 1){
+            return 0;
+        }
+        int minCutTimes = 0;
+        char[] chars = s.toCharArray();
+        //动态规划预处理
+        boolean dp[][] = new boolean[len][len];
+        for (int j = 0; j < len; j++) {
+            for (int i = 0; i <= j; i++) {
+                if (i == j){
+                    dp[i][j] = true;
+                }
+                else if(j-i == 1 & chars[i]==chars[j]){
+                    dp[i][j] = true;
+                }else if (j-i > 1 & dp[i+1][j-1] & chars[i]==chars[j]){
+                    dp[i][j] = true;
+                }
+            }
+        }
+        //贪心法去找每一个序列的最大回文子序列
+        //贪心的方法：找出以startIndex为开始的最大回文序列，在不断迭代
+        //错误。
+        int startIndex = 0;
+        int endIndex = len - 1;
+        while (startIndex < len){
+            for (int j = endIndex; j >= startIndex; j--){
+                if (dp[startIndex][j]){
+                    startIndex = j + 1;
+                    if (startIndex < len){
+                        minCutTimes++;
+                    }
+                    break;
+                }
+            }
+        }
+        return minCutTimes;
+    }
 
 }
