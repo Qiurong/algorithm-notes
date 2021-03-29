c5bf176709c22274ef2a031b89bd2c0c097d1214
Qiurong <rongqiu_carl@qq.com>
2021-03-29 10:48:22 +0800

    update longest-increasing-subsequence with correct sloution

--- dynamic_programming/longest_increasing_subsequence.java
+++ dynamic_programming/longest_increasing_subsequence.java
@@ -6,46 +6,27 @@ package dynamic_programming;
  * @create: 2021-03-17 22:42
  **/
 public class longest_increasing_subsequence {
-    //从后往前迭代, 算法错误.看了官方题解应该是从前往后迭代.
+    //从前往后迭代
+    //时间复杂度: O(n^2), 空间复杂度：O(n)
     public int lengthOfLIS_brutalForce(int[] nums) {
         int len = nums.length;
-        //用于记录最长递增子序列的数值. dp[i] = nums[i...len-1]的最长递增子序列长度
+        int maxlen = 1;
+        //用于记录最长递增子序列的数值. dp[i] = nums[i...len-1]的最长递增子序列长度.
+        // 这个子序列必须包含nums[i]! (满足了动态规划 无后效性 的设计思想)
         int dp[] = new int[len];
-        //用于记录最长递增子序列的最小值. dp[i] = nums[i...len-1]的最长递增子序列的最小值
-        int min[] = new int[len];
-        //边界
-        dp[len-1] = 1;
-        min[len-1] = nums[len-1];
-        for (int i = len - 2; i >= 0; i--) {
-            if (nums[i] < min[i+1]){
-                dp[i] = dp[i+1] + 1;
-                min[i] = nums[i];
-            }
-            else {
-                boolean flag = false;
-                for (int j = i+2; j <= len-1 ; j++) {
-                    if (nums[i] < min[j]){
-                        int temp = dp[j] + 1;
-                        if (temp > dp[i+1]){
-                            dp[i] = temp;
-                            min[i] = nums[i];
-                        } else if (temp < dp[i+1]){
-                            dp[i] = dp[i+1];
-                            min[i] = nums[i];
-                        } else{
-                            dp[i] = dp[i+1];
-                            min[i] = Math.max(nums[i],nums[i]);
-                        }
-                        flag = true;
-                        break;
-                    }
-                }
-                if(!flag){
-                    dp[i] = dp[i+1];
-                    min[i] = min[i+1];
+        //初始化
+        dp[0] = 1;
+        for (int i = 1; i < len; i++) {
+            //在这里给dp[i]赋初值以应对
+            dp[i] = 1;
+            //dp[i] = max{dp[j]+1} if nums[j] < nums[i]
+            for (int j = 0; j <= i; j++) {
+                if (nums[i] > nums[j]){
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
                 }
             }
+            maxlen = Math.max(dp[i], maxlen);
         }
-        return dp[0];
+        return maxlen;
     }
 }
