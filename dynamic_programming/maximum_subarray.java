7e33ce3f78571aaa24c545dcd28a1c8273be9a84
Qiurong <rongqiu_carl@qq.com>
2021-03-29 13:48:35 +0800

    update maximum-subarray

--- dynamic_programming/maximum_subarray.java
+++ dynamic_programming/maximum_subarray.java
@@ -6,8 +6,9 @@ package dynamic_programming;
  * @create: 2021-03-29 11:10
  **/
 public class maximum_subarray {
+    //时间复杂度:O(n), 空间复杂度:O(n)
     public int maxSubArray(int[] nums) {
-        int len = nums.length - 1;
+        int len = nums.length;
         int maxSum = nums[0];
         int currMax[] = new int[len];
         currMax[0] = nums[0];
@@ -18,6 +19,23 @@ public class maximum_subarray {
                 currMax[i] = currMax[i-1] + nums[i];
             }
             maxSum = Math.max(maxSum,currMax[i]);
+        }
+        return maxSum;
+    }
+
+    //由于dp计算过程中dp[i]只与dp[i-1]有关，可以用滚动数组思想把时间复杂度降到O(1)
+    public int maxSubArray_WithLessSpace(int[] nums) {
+        int len = nums.length;
+        int maxSum = nums[0];
+        int preMax = nums[0];
+        //int currMax = nums[0];
+        for (int i = 1; i < len; i++) {
+            if (preMax  < 0){
+                preMax = nums[i];
+            }else {
+                preMax += nums[i];
+            }
+            maxSum = Math.max(maxSum,preMax);
         }
         return maxSum;
     }
