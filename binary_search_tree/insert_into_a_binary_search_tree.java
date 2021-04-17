323b4a30e13b4f83ef89fa6a67d022bb2d13a107
Qiurong <rongqiu_carl@qq.com>
2021-04-17 16:45:05 +0800

    add insert-into-a-binary-search-tree

---
+++ binary_search_tree/insert_into_a_binary_search_tree.java
@@ -0,0 +1,32 @@
+package binary_search_tree;
+
+/**
+ * @description:
+ * @author: Qr
+ * @create: 2021-04-17 16:23
+ **/
+public class insert_into_a_binary_search_tree {
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        if (root == null){
+            return new TreeNode(val);
+        }
+        TreeNode prev = root;   //中序遍历中插入节点的前一个结点
+        TreeNode curr = root;   //最后插入的结点位置
+        //寻找插入位置
+        while (curr != null){
+            prev = curr;
+            if (curr.val > val){
+                curr = curr.left;
+            } else {
+                curr = curr.right;
+            }
+        }
+        //插入结点
+        if (prev.val > val){
+            prev.left = new TreeNode(val);
+        }else {
+            prev.right = new TreeNode(val);
+        }
+        return root;
+    }
+}
