01ee2d16ceb408c900a1eea9dc252b00c8fcc33d
Qiurong <rongqiu_carl@qq.com>
2021-04-19 13:49:56 +0800

    update permutations

--- backtrack/permutations.java
+++ backtrack/permutations.java
@@ -2,7 +2,6 @@ package backtrack;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashSet;
 import java.util.List;
 
 /**
@@ -14,34 +13,35 @@ public class permutations {
 
     //难点：回溯的下一步如何处理。
     //根据题目要求，不能填写已经标记过的数，因此用一个标记数组来标记是否填过
-    static List<List<Integer>> res = new ArrayList<>();
-    static List<Integer> path = new ArrayList<>();
-    static boolean[] visit;
+    List<List<Integer>> res = new ArrayList<>();
+    List<Integer> path = new ArrayList<>();
+    boolean[] visit;
 
-    public static List<List<Integer>> permute(int[] nums) {
+    public List<List<Integer>> permute(int[] nums) {
         visit = new boolean[nums.length];
         Arrays.fill(visit,false);
         backtrack(0,nums);
         return res;
     }
     //index代表当前取得下标
-    public static void backtrack(int depth, int[] nums){
+    public void backtrack(int depth, int[] nums){
         if (depth == nums.length){
             res.add(new ArrayList<>(path));
             return;
         }
 
-        //每次去遍历所有数组
+        //排列问题每一步的选择列表都是从0开始的，而组合/子集问题从start开始
         for (int i = 0; i < nums.length; i++) {
-            //搜索到没有visit的则visit
-            if (!visit[i]){
-                path.add(nums[i]);
-                visit[i] = true;
-                //这里更新时更新depth参数
-                backtrack(depth+1,nums);
-                visit[i] = false;
-                path.remove(path.size() - 1);
+            //visit则剪枝
+            if (visit[i]){
+                continue;
             }
+            path.add(nums[i]);
+            visit[i] = true;
+            //这里更新时更新depth参数
+            backtrack(depth+1,nums);
+            visit[i] = false;
+            path.remove(path.size() - 1);
         }
     }
 
